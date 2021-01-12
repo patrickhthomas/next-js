8475640b5da4c28b799daeb7b375a701f6dbc597
Patrick <patrick@sendhelp.biz>
2021-01-12 09:43:12 -0500

    first deploy

--- splash/pages/index.js
+++ splash/pages/index.js
@@ -18,15 +18,16 @@ export default function Home() {
 
       <div className="card">
         <img className="big-logo"src="assets/big-logo.svg" alt=""></img>
+        <div className="blur"></div>
       </div>
     
       <div className="card diamond">
         <img className="d-end" src="assets/mobile/top.png" alt=""/>
         <img className="tr" src="assets/mobile/top-right.png" alt="" />
         <img className="tl" src="assets/mobile/top-left.png" alt="" />
-        <h4 className="rel">Tea and stuff</h4>
+        <h4 className="rel">Premium Teas</h4>
         <span className="break"></span>
-        <h4 className="rel">Tea and stuff</h4>
+        <h4 className="rel">Natural Bodycare</h4>
         <img className="br" src="assets/mobile/bottom-right.png" alt="" />
         <img className="bl" src="assets/mobile/bottom-left.png" alt="" />
         <img className="btm" src="assets/mobile/bottom.png" alt="" />
@@ -34,11 +35,26 @@ export default function Home() {
       </div>
     
       <div className="card">
+        <p>We’re not quite ready to announce a grand opening date, but we’ll post all the details on this page as soon as we are! Until then, there are a few things you can do to make sure you stay in loop.</p>
       </div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
+      <div className="card">
+        <h4>Follow us on social media</h4>
+        {/*FB logo link | IG logo link*/}
+      </div>
+      <div className="card">
+        <h4>Read about us in the news</h4>
+        <div className="news"></div>
+        <div className="news"></div>
+        <div className="news"></div>
+      </div>
+      <div className="card">
+        <h4>Shop online</h4>
+        <button></button>
+      </div>
+      <div className="card">
+        <h4>Get in touch</h4>
+        <button></button>
+      </div>
       <div className="card"></div>
 
     </div>
